372ef35d0c1bc4deb322778900c09269df93e9cc
mingyuan-f <1119369173@qq.com>
2020-11-12 20:24:34 +0800

    events. Actor Model

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::future::Future;
 use std::option::Option::Some;
 
@@ -5,11 +6,24 @@ use async_std::net::{TcpListener, TcpStream, ToSocketAddrs};
 use async_std::sync::Arc;
 use async_std::task;
 use async_std::task::JoinHandle;
-use futures::{AsyncBufReadExt, AsyncWriteExt, StreamExt};
-use futures::channel::mpsc::UnboundedReceiver;
+use futures::{AsyncBufReadExt, AsyncWriteExt, SinkExt, StreamExt};
+use futures::channel::mpsc::{UnboundedReceiver, UnboundedSender};
+use futures::channel::mpsc;
 use futures::io::BufReader;
 
-type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
+
+enum Event {
+    NewPeer {
+        name: String,
+        stream: Arc<TcpStream>,
+    },
+    Message {
+        from: String,
+        to: Vec<String>,
+        msg: String,
+    },
+}
 
 async fn spawn_and_log_error(fut: impl Future<Output=Result<()>> + Send + 'static) -> JoinHandle<()> {
     task::spawn(async {
@@ -19,22 +33,48 @@ async fn spawn_and_log_error(fut: impl Future<Output=Result<()>> + Send + 'stati
     })
 }
 
-async fn connection_loop(stream: Arc<TcpStream>) -> Result<()> {
+async fn broker_loop(mut events: UnboundedReceiver<Event>) -> Result<()> {
+    let mut peers = HashMap::<String, UnboundedSender<String>>::new();
+    while let Some(event) = events.next().await {
+        match event {
+            Event::NewPeer { name, stream } => {
+                println!("New user connected. name: {}", name);
+                let (sender, receiver) = mpsc::unbounded();
+                peers.insert(name, sender);
+                let _connection_writer_loop = spawn_and_log_error(connection_writer_loop(receiver, stream));
+            }
+            Event::Message { from, to, msg } => {
+                let msg = format!("from {}: {}", from, msg);
+                for to in to {
+                    if let Some(sender) = peers.get_mut(&to) {
+                        sender.send(msg.clone()).await?
+                    }
+                }
+            }
+        }
+    }
+    Ok(())
+}
+
+async fn connection_loop(stream: TcpStream, mut events_sender: UnboundedSender<Event>) -> Result<()> {
+    let stream = Arc::new(stream);
     let reader = BufReader::new(&*stream);
     let mut lines = reader.lines();
     let name = match lines.next().await {
         None => { Err("peer disconnected immediately")? }
         Some(line) => { line? }
     };
-    println!("New user connected. name: {}", name);
+    events_sender.send(Event::NewPeer { name: name.clone(), stream: stream.clone() }).await?;
+
     while let Some(line) = lines.next().await {
         let line = line?;
         let (dest, msg) = match line.find(":") {
             None => { continue; }
             Some(idx) => { (&line[0..idx], line[idx + 1..].trim()) }
         };
-        let _dest: Vec<String> = dest.split(",").map(|dest| dest.trim().to_string()).collect();
-        let _msg = msg.to_string();
+        let to: Vec<String> = dest.split(",").map(|dest| dest.trim().to_string()).collect();
+        let msg = msg.to_string();
+        events_sender.send(Event::Message { from: name.clone(), to, msg }).await?;
     }
     Ok(())
 }
@@ -50,10 +90,12 @@ async fn connection_writer_loop(mut messages: UnboundedReceiver<String>, stream:
 async fn accept_loop(addr: impl ToSocketAddrs) -> Result<()> {
     let listener = TcpListener::bind(addr).await?;
     let mut incoming = listener.incoming();
+    let (events_sender, events_receiver) = mpsc::unbounded();
+    let _broker_loop = task::spawn(broker_loop(events_receiver));
     while let Some(stream) = incoming.next().await {
-        let stream = Arc::new(stream?);
-        let handle = spawn_and_log_error(connection_loop(stream.clone()));
-        handle.await;
+        let stream = stream?;
+        println!("Accepting from: {}", stream.peer_addr()?);
+        let _connection_loop = spawn_and_log_error(connection_loop(stream, events_sender.clone()));
     }
     Ok(())
 }
