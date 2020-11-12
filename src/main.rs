c2f7ac979cc3af48b8a8c12ef1d6352df601385c
mingyuan-f <1119369173@qq.com>
2020-11-12 17:09:14 +0800

    connection_writer_loop

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use std::future::Future;
+use std::option::Option::Some;
 
 use async_std::net::{TcpListener, TcpStream, ToSocketAddrs};
+use async_std::sync::Arc;
 use async_std::task;
 use async_std::task::JoinHandle;
-use futures::{AsyncBufReadExt, StreamExt};
+use futures::{AsyncBufReadExt, AsyncWriteExt, StreamExt};
+use futures::channel::mpsc::UnboundedReceiver;
 use futures::io::BufReader;
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
@@ -16,8 +19,8 @@ async fn spawn_and_log_error(fut: impl Future<Output=Result<()>> + Send + 'stati
     })
 }
 
-async fn connection_loop(stream: TcpStream) -> Result<()> {
-    let reader = BufReader::new(stream);
+async fn connection_loop(stream: Arc<TcpStream>) -> Result<()> {
+    let reader = BufReader::new(&*stream);
     let mut lines = reader.lines();
     let name = match lines.next().await {
         None => { Err("peer disconnected immediately")? }
@@ -36,12 +39,20 @@ async fn connection_loop(stream: TcpStream) -> Result<()> {
     Ok(())
 }
 
+async fn connection_writer_loop(mut messages: UnboundedReceiver<String>, stream: Arc<TcpStream>) -> Result<()> {
+    while let Some(message) = messages.next().await {
+        let mut stream = &*stream;
+        stream.write_all(message.as_bytes()).await?
+    }
+    Ok(())
+}
+
 async fn accept_loop(addr: impl ToSocketAddrs) -> Result<()> {
     let listener = TcpListener::bind(addr).await?;
     let mut incoming = listener.incoming();
     while let Some(stream) = incoming.next().await {
-        let stream = stream?;
-        let handle = spawn_and_log_error(connection_loop(stream));
+        let stream = Arc::new(stream?);
+        let handle = spawn_and_log_error(connection_loop(stream.clone()));
         handle.await;
     }
     Ok(())
