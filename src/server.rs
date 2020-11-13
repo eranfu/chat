a30de7b022f2884e2c6c452e1c22a934f539d2b9
mingyuan-f <1119369173@qq.com>
2020-11-13 22:15:39 +0800

    id

--- src/server.rs
+++ src/server.rs
@@ -1,96 +1,78 @@
 use std::collections::HashMap;
+use std::option::Option::Some;
+use std::sync::atomic::{AtomicUsize, Ordering};
 
 use async_std::io::BufReader;
-use async_std::net::{TcpListener, ToSocketAddrs};
+use async_std::net::{Shutdown, TcpListener, ToSocketAddrs};
+use bimap::{BiHashMap, Overwritten};
 use futures::{AsyncBufReadExt, AsyncWriteExt, SinkExt, StreamExt};
 use futures::channel::mpsc;
-use futures::select;
 
 use crate::core::*;
 
-enum Disconnect {}
-
 enum Event {
     NewPeer {
+        id: usize,
         name: String,
         stream: Arc<TcpStream>,
-        disconnect_receiver: Receiver<Disconnect>,
+    },
+    Disconnected {
+        id: usize,
     },
     Message {
-        from: String,
+        from_id: usize,
         to: Vec<String>,
         msg: String,
     },
 }
 
-async fn broker_loop(events: Receiver<Event>) {
-    let mut peers = HashMap::<String, Sender<String>>::new();
-    let mut writer_handles = HashMap::<String, JoinHandle<()>>::new();
-    let (writer_shutdown_sender, writer_shutdown_receiver) = mpsc::unbounded::<String>();
-    let mut writer_shutdown_receiver = writer_shutdown_receiver.fuse();
-    let mut events = events.fuse();
-    loop {
-        let event = select! {
-            writer_shutdown = writer_shutdown_receiver.next() => match writer_shutdown {
-                None => { unreachable!(); },
-                Some(writer_shutdown) => {
-                    let name = writer_shutdown;
-                    assert!(peers.remove(&name).is_some());
-                    continue;
-                },
-            },
-            event = events.next() => match event {
-                None => { break; },
-                Some(event) => { event },
-            },
-        };
+async fn broker_loop(mut events: Receiver<Event>) {
+    let mut id_name_pairs = BiHashMap::<usize, String>::new();
+    let mut peers = HashMap::<usize, Sender<String>>::new();
+    let mut writer_handles = HashMap::<usize, JoinHandle<()>>::new();
+    while let Some(event) = events.next().await {
         match event {
-            Event::NewPeer {
-                name,
-                stream,
-                disconnect_receiver,
-            } => {
+            Event::NewPeer { id, name, stream, } => {
                 println!("New user connected. name: {}", name);
-                if let Some(handle) = writer_handles.remove(&name) {
-                    handle.cancel().await;
-                };
-
-                let (message_sender, mut message_receiver) = mpsc::unbounded();
-                {
-                    let mut writer_shutdown_sender = writer_shutdown_sender.clone();
-                    let name = name.clone();
-                    writer_handles.insert(
-                        name.clone(),
-                        spawn_and_log_error(async move {
-                            connection_writer_loop(disconnect_receiver, &mut message_receiver, stream).await?;
-                            writer_shutdown_sender.send(name).await?;
-                            Ok(())
-                        }),
-                    );
-                }
-                peers.insert(name.clone(), message_sender);
-            }
-            Event::Message { from, to, msg } => {
-                let msg = format!("from {}: {}\n", from, msg);
-                for to in to {
-                    if let Some(sender) = peers.get_mut(&to) {
-                        await_and_log_error(sender.send(msg.clone())).await;
+                match id_name_pairs.insert(id, name) {
+                    Overwritten::Neither => {}
+                    Overwritten::Right(id, name) => {
+                        peers.remove(&id);
+                        writer_handles.remove(&id).unwrap().await;
+                        println!("{} disconnected by duplicate.", name);
                     }
+                    _ => { unreachable!(); }
+                }
+
+                let (message_sender, message_receiver) = mpsc::unbounded();
+                peers.insert(id, message_sender);
+                let writer_handle = spawn_and_log_error(connection_writer_loop(message_receiver, stream));
+                writer_handles.insert(id, writer_handle);
+            }
+            Event::Disconnected { id } => {
+                if let Some((id, name)) = id_name_pairs.remove_by_left(&id) {
+                    peers.remove(&id).unwrap();
+                    writer_handles.remove(&id).unwrap().await;
+                    println!("{} disconnected.", name);
+                }
+            }
+            Event::Message { from_id, to, msg } => {
+                let from = id_name_pairs.get_by_left(&from_id).unwrap();
+                let msg = format!("from {}: {}\n", from, msg);
+                for to_id in to.into_iter().filter_map(|to_name| id_name_pairs.get_by_right(&to_name)) {
+                    await_and_log_error(peers.get_mut(to_id).unwrap().send(msg.clone())).await
                 }
             }
         };
     }
 
     drop(peers);
-    drop(writer_shutdown_sender);
-    drop(writer_handles);
-    while let Some(_) = writer_shutdown_receiver.next().await {}
+    for (_name, writer_handle) in writer_handles {
+        writer_handle.await;
+    }
 }
 
-async fn connection_loop(
-    stream: TcpStream,
-    mut events_sender: Sender<Event>,
-) -> Result<()> {
+async fn connection_loop(stream: TcpStream, mut events_sender: Sender<Event>) -> Result<()> {
     let stream = Arc::new(stream);
     let reader = BufReader::new(&*stream);
     let mut lines = reader.lines();
@@ -98,22 +80,16 @@ async fn connection_loop(
         None => Err("peer disconnected immediately")?,
         Some(line) => line?,
     };
-    let (_disconnect_sender, disconnect_receiver) = mpsc::unbounded();
-    events_sender
-        .send(Event::NewPeer {
-            name: name.clone(),
-            stream: stream.clone(),
-            disconnect_receiver,
-        })
-        .await
-        .unwrap();
+
+    static ID: AtomicUsize = AtomicUsize::new(0);
+    let id = ID.fetch_add(1, Ordering::Relaxed);
+
+    events_sender.send(Event::NewPeer { id, name: name.clone(), stream: stream.clone() }).await.unwrap();
 
     while let Some(line) = lines.next().await {
         let line = line?;
         let (dest, msg) = match line.find(":") {
-            None => {
-                continue;
-            }
+            None => continue,
             Some(idx) => (&line[0..idx], line[idx + 1..].trim()),
         };
         let to: Vec<String> = dest
@@ -121,40 +97,19 @@ async fn connection_loop(
             .map(|dest| dest.trim().to_string())
             .collect();
         let msg = msg.to_string();
-        events_sender
-            .send(Event::Message {
-                from: name.clone(),
-                to,
-                msg,
-            })
-            .await
-            .unwrap();
+        events_sender.send(Event::Message { from_id: id, to, msg }).await.unwrap();
     }
+
+    events_sender.send(Event::Disconnected { id }).await.unwrap();
     Ok(())
 }
 
-async fn connection_writer_loop(
-    disconnect_receiver: Receiver<Disconnect>,
-    message_receiver: &mut Receiver<String>,
-    stream: Arc<TcpStream>,
-) -> Result<()> {
-    let mut messages = message_receiver.fuse();
-    let mut disconnect_receiver = disconnect_receiver.fuse();
-    loop {
-        select! {
-            message = messages.next() => match message {
-                None => { break; },
-                Some(message) => {
-                    let mut stream = &*stream;
-                    stream.write_all(message.as_bytes()).await?;
-                },
-            },
-            disconnect = disconnect_receiver.next() => match disconnect {
-                None => { break; },
-                Some(_) => { unreachable!(); }
-            }
-        }
+async fn connection_writer_loop(mut message_receiver: Receiver<String>, stream: Arc<TcpStream>) -> Result<()> {
+    while let Some(message) = message_receiver.next().await {
+        let mut stream = &*stream;
+        stream.write_all(message.as_bytes()).await?;
     }
+    stream.shutdown(Shutdown::Both)?;
     Ok(())
 }
 
